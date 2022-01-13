668fc88972859101b1a7322c673f9c6c6b2ce336
Andre Staltz <andre@staltz.com>
2022-01-13 11:50:03 +0200

    add tests lol

---
+++ test.js
@@ -0,0 +1,52 @@
+const test = require('tape');
+const pull = require('pull-stream');
+const xsFromPullStream = require('./index').default;
+
+test('converts pull-stream msg to xstream next()', (t) => {
+  t.plan(4);
+  const expected = [1, 2, 3];
+  const stream = xsFromPullStream(pull.values([1, 2, 3]));
+  stream.addListener({
+    next: (x) => {
+      t.equal(x, expected.shift());
+    },
+    error: (err) => {
+      t.fail(err);
+    },
+    complete: () => {
+      t.pass('complete');
+    },
+  });
+});
+
+test('converts pull-stream error to xstream error()', (t) => {
+  t.plan(1);
+  const stream = xsFromPullStream(pull.error(new Error('badbad')));
+  stream.addListener({
+    next: (x) => {
+      t.fail('should not be called');
+    },
+    error: (err) => {
+      t.equal(err.message, 'badbad');
+    },
+    complete: () => {
+      t.fail('should not be called');
+    },
+  });
+});
+
+test('converts pull-stream end to xstream complete()', (t) => {
+  t.plan(1);
+  const stream = xsFromPullStream(pull.empty());
+  stream.addListener({
+    next: (x) => {
+      t.fail('should not be called');
+    },
+    error: (err) => {
+      t.fail('should not be called');
+    },
+    complete: () => {
+      t.pass('complete');
+    },
+  });
+});
